cdd27e246b526c35bc1b1fff3dd86964d4b434fb
friedcrumpets <jamie@jamiemullis.com>
2022-12-17 16:51:37 +0000

    feat(Items): Monobehaviours that contain a list of Options that can infinitely extend an items ability to do a task - Are Interfaces and Inheritance the correct way to do this or are they limiting??

--- Assets/Blueprints/Items/Item.cs
+++ Assets/Blueprints/Items/Item.cs
@@ -1,30 +1,86 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blueprints.Command;
 using UnityEngine;
 
-namespace Blueprints.Inventory
+namespace Blueprints.Items
 {
     [Serializable]
     public abstract class Item : MonoBehaviour
     {
-        string Name { get; set; }
-        
-        public abstract void Collect(); 
-        
-        public abstract void Discard();
+        public List<Option<Item>> Options { get; set; }
+
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public void AddOption(Option<Item> option) => Options.Add(option);
+        public bool RemoveOption(Option<Item> option) => Options.Remove(option);
+
+        public bool RemoveOption(string optionName)
+        {
+            var first = Options.FirstOrDefault(option => option.DisplayName.Contains(optionName));
+            return Options.Remove(first);
+        }
     }
 
+    public interface IDroppable
+    {
+        void Drop(Item item);
+    }
+
+    public interface IDestroyable
+    {
+        void Destroy(Item item);
+    }
+    
     public interface IUsable
     {
         bool Used { get; set; }
 
-        void Use();
+        void Use(Item item);
     }
 
     public interface IEquippable
     {
         bool Equipped { get; set; }
 
-        void Equip();
-        void UnEquip();
+        void Equip(Item item);
+        void UnEquip(Item item);
     }
+
+    public interface IExamineable
+    {
+        bool CanExamine { get; set; }
+
+        void Examine();
+    }
+
+    /// <summary>
+    /// Provides the ability to reclaim an item upon death. Similar mechanic implemented by Runescape
+    /// </summary>
+    public interface IReclaimable
+    {
+        bool CanReclaim { get; set; }
+    }
+    
+    public interface ITradeable  
+    {
+        IEnumerable<Item> Value { get; set; }
+        
+        /*
+         I NEED a lot more information on this before I can figure this out. 
+         Trading is an entire system separate to Items
+         // TODO: Investigate trading systems within games to discover mechanics
+         
+         Tradeable items aren't always traded with coin, coins/items... are coins an item
+        
+         Items can be traded with ITradeable, but the vendor supplies the logic to buy and sell the item.
+         
+         I Think the cost for an item is better reserved for a Cost lookup table. 
+         Similar to a drop table In a way. This can be an API styled json file that's received containing cost information.
+         */
+    }
+
+    // Interaction extension interfaces should become available also
 }
