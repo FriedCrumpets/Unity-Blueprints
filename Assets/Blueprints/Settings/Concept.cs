1ae75e34f29bad0ee8e4c0968d986adef0987589
friedcrumpets <jamie@jamiemullis.com>
2022-12-17 16:38:56 +0000

    chore(Naming): Renamed 'Storage' to 'cachedStorage' for clarity

--- Assets/Blueprints/Settings/Concept.cs
+++ Assets/Blueprints/Settings/Concept.cs
@@ -1,32 +1,30 @@
-using UnityEngine;
-
 namespace Blueprints.Settings
 {
     [System.Serializable]
     public class Concept
     {
-        public Concept_SO storage;
+        public Concept_SO localStorage;
 
         public float Setting1
         {
-            get => storage.Setting1;
+            get => localStorage.Setting1;
             set
             {
                 // do things
-                storage.Setting1 = value;
+                localStorage.Setting1 = value;
             }
         }
 
         public string Setting2
         {
-            get => storage.Setting2;
-            set => storage.Setting2 = value;
+            get => localStorage.Setting2;
+            set => localStorage.Setting2 = value;
         }
 
         public bool Setting3
         {
-            get => storage.Setting3;
-            set => storage.Setting3 = value;
+            get => localStorage.Setting3;
+            set => localStorage.Setting3 = value;
         }
     }
 }
